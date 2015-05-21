40d52c966d0f04df55f4aa159fa08dc2a0455d80
Erika Jonikaite <erikajon@hotmail.co.uk>
2015-05-21 16:32:49 +0100

    refactored bike creation using let

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,21 +1,22 @@
 require_relative '../lib/bike'
 
 describe Bike do
+
+  let(:bike) { Bike.new }
+
   it 'should not be broken when created' do
-    bike = Bike.new
     expect(bike.broken?).to be false
   end
 
   it 'should be able to break' do
-    bike = Bike.new
     bike.break
     expect(bike.broken?).to be true
   end
 
   it 'should be able get fixed' do
-    bike = Bike.new
     bike.break
     bike.fix
     expect(bike.broken?).to be false
   end
+
 end
