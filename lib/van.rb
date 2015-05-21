ba3ee35fef0668ae6c8de37d60ef1bf626edb2c7
Erika Jonikaite <erikajon@hotmail.co.uk>
2015-05-21 20:31:07 +0100

    Van knows when it's full now

--- lib/van.rb
+++ lib/van.rb
@@ -1,6 +1,8 @@
 class Van
   DEFAULT_CAPACITY = 10
-  
+
+  attr_accessor :capacity, :bikes
+
   def initialize(options = {})
     @capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
     @bikes = []
@@ -16,5 +18,9 @@ class Van
 
   def drop_off_bike(bike)
     @bikes.delete(bike)
+  end
+
+  def full?
+    @capacity <= @bikes.count
   end
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,10 +1,11 @@
+require 'pry'
 require_relative '../lib/van'
 
 describe Van do
 
-  let(:station) { DockStation.new(:capacity => 20) }
+  let(:station) { DockStation.new }
   let(:bike) { Bike.new }
-  let(:van) { Van.new(:capacity => 10)}
+  let(:van) { Van.new(:capacity => 20)}
 
   it 'should be able to pick up' do
     station.dock(bike)
@@ -19,4 +20,9 @@ describe Van do
     expect(van.bike_count).to eq 0
   end
 
+  it 'should know when it\'s full' do
+    expect(van.full?).to be false
+    50.times { van.pick_up_bike(Bike.new) }
+    expect(van.full?).to be true
+  end
 end
