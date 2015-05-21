2c9cf541b85bb13bb45cd3bb31235d41b24c2fc6
Erika Jonikaite <erikajon@hotmail.co.uk>
2015-05-21 19:07:26 +0100

    Van can drop off bikes

--- lib/van.rb
+++ lib/van.rb
@@ -12,4 +12,8 @@ def pick_up_bike bike
   @bikes << bike
 end
 
+def drop_of_bike bike
+  @bikes.delete(bike)
+end
+
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -10,6 +10,16 @@ describe Van do
     expect(van.bike_count).to eq 1
   end
 
-  # it 'should be able to drop off a bike'
-  # van
+  it 'should be able to drop off a bike' do
+    van = Van.new
+    bike = Bike.new
+    van.pick_up_bike(bike)
+    expect(van.bike_count).to eq 1
+    van.drop_of_bike(bike)
+    expect(van.bike_count).to eq 0
+  end
+
+
+
+
 end
